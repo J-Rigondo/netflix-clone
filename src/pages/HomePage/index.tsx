ab0b9cfa71f537b001ebb1f5ffdfe1f7568e24b9
Park Jun Hyeong <futuregoing@prismweaver.com>
2022-06-06 13:46:42 +0900

    슬라이더 추가

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import { fetchPlayingMovies } from 'api/movie';
-import { Wrapper, Loading, Banner, Title, Overview } from './styled';
+import {
+  Box,
+  Wrapper,
+  Loading,
+  Banner,
+  Title,
+  Overview,
+  Row,
+  Slider,
+  rowVariants,
+} from './styled';
 import { makeImagePath } from 'lib/utils/common';
+import { AnimatePresence } from 'framer-motion';
+
+const offset = 6;
 
 const HomePage = () => {
+  const [step, setStep] = useState(0);
   const { isLoading, data, isError } = useQuery(
     'fetchPlayingMovies',
     fetchPlayingMovies,
   );
+
+  const stepUp = () => {
+    if (data) {
+      const totalMovies = data.results.length;
+      const maxIndex = Math.floor(totalMovies / offset);
+      setStep((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+
+  const sliderMovies = (() => {
+    if (!data) return [];
+
+    return data.results.slice(1).slice(offset * step, offset * step + offset);
+  })();
 
   if (isError) return <div>Error! please refresh</div>;
   if (isLoading) return <Loading>...Loading</Loading>;
@@ -19,10 +47,29 @@ const HomePage = () => {
 
   return (
     <Wrapper>
-      <Banner bgPhoto={makeImagePath(backdrop_path)}>
+      <Banner onClick={stepUp} bgphoto={makeImagePath(backdrop_path)}>
         <Title>{title}</Title>
         <Overview>{overview}</Overview>
       </Banner>
+      <Slider>
+        <AnimatePresence initial={false}>
+          <Row
+            key={step}
+            variants={rowVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            transition={{ type: 'tween', duration: 1 }}
+          >
+            {sliderMovies.map((movie) => (
+              <Box
+                key={movie.id}
+                bgphoto={makeImagePath(movie.backdrop_path, 'w500')}
+              />
+            ))}
+          </Row>
+        </AnimatePresence>
+      </Slider>
     </Wrapper>
   );
 };
--- src/pages/HomePage/styled.ts
+++ src/pages/HomePage/styled.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 export const Wrapper = styled.div`
   height: 200vh;
+  background-color: black;
+  overflow-x: hidden;
 `;
 
 export const Loading = styled.div`
@@ -11,7 +14,7 @@ export const Loading = styled.div`
   justify-content: center;
 `;
 
-export const Banner = styled.div<{ bgPhoto: string }>`
+export const Banner = styled.div<{ bgphoto: string }>`
   height: 100vh;
   display: flex;
   flex-direction: column;
@@ -19,17 +22,51 @@ export const Banner = styled.div<{ bgPhoto: string }>`
   justify-content: center;
   padding: 60px;
   background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
-    url(${(props) => props.bgPhoto});
+    url(${(props) => props.bgphoto});
   background-size: cover;
   color: white;
 `;
 
 export const Title = styled.h2`
   font-size: 38px;
-  margin-bottom: 20px;
+  margin-bottom: 40px;
 `;
 
 export const Overview = styled.p`
-  font-size: 26px;
+  font-size: 20px;
   width: 50vw;
+  line-height: 30px;
+`;
+
+export const Slider = styled.div`
+  position: relative;
+  top: -20%;
+`;
+
+export const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 10,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth - 10,
+  },
+};
+
+export const Row = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 10px;
+  position: absolute;
+  width: 100%;
+`;
+
+export const Box = styled(motion.div)<{ bgphoto: string }>`
+  background-color: white;
+  height: 200px;
+  background-image: url(${(props) => props.bgphoto});
+  background-size: cover;
+  background-position: center center;
 `;
